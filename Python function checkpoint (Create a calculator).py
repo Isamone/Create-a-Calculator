62f82621ed77d5c093b7d26abe96fe9a662e8c6d
Isamone <idamcida@yahoo.com>
2025-05-01 19:46:26 +0100

    Update Python function checkpoint (Create a calculator).py

--- Python function checkpoint (Create a calculator).py
+++ Python function checkpoint (Create a calculator).py
@@ -4,20 +4,18 @@ Create a simple calculator application in Python that can perform basic arithmet
 
 Description:
 
-This exercise involves creating a simple calculator app using Python. The app should be able to perform addition, subtraction, multiplication, and division operations. It will prompt the user to input two numbers and an arithmetic operator, then perform the corresponding operation on the numbers and return the result.
+This exercise involves creating a simple calculator app using Python. The app should be able to perform addition, subtraction, multiplication, and division operations. 
+It will prompt the user to input two numbers and an arithmetic operator, then perform the corresponding operation on the numbers and return the result.
 
 Example :
 
 Input:
 
 Enter the first number: 10
-
 Enter the second number: 5
-
 Enter the operator (+, -, *, /): *
 
 Output:
-
 The result is: 50
 
 
